517dae2e02671a70f4c4294f3ba789d967694855
antiloplastico <albertorob98@gmail.com>
2023-02-12 14:07:10 +0100

    Update utils.py

--- assignment2/utils.py
+++ assignment2/utils.py
@@ -26,6 +26,8 @@ def batch_loader(
     indices = list(range(len(X)))
 
     # TODO (copy from last assignment) implement dataset shuffling here.
+    if shuffle:
+         np.random.shuffle(indices)
 
     for i in range(num_batches):
         # select a set of indices for each batch of samples
