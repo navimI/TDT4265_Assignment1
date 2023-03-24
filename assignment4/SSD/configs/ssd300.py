38cfe334c0c6206417f83b1827dde2a893329996
navimI <ivan.garciaa@um.es>
2023-03-24 14:51:21 +0100

    Number of it and adam opti

--- assignment4/SSD/configs/ssd300.py
+++ assignment4/SSD/configs/ssd300.py
@@ -14,7 +14,7 @@ train = dict(
     amp=True,  # Automatic mixed precision
     log_interval=20,
     seed=0,
-    epochs=30,
+    epochs=15,
     _output_dir=get_output_dir(),
     imshape=(300, 300),
     image_channels=3
@@ -24,7 +24,7 @@ anchors = L(AnchorBoxes)(
     feature_sizes=[[38, 38], [19, 19], [10, 10], [5, 5], [3, 3], [1, 1]],
     # Strides is the number of pixels (in image space) between each spatial position in the feature map
     strides=[[8, 8], [16, 16], [32, 32], [64, 64], [100, 100], [300, 300]],
-    min_sizes=[[30, 30], [60, 60], [111, 111], [162, 162], [213, 213], [264, 264], [315, 315]],
+    min_sizes=[[15, 15], [30, 30], [111, 111], [162, 162], [213, 213], [264, 264], [315, 315]],
     # aspect ratio is defined per feature map (first index is largest feature map (38x38))
     # aspect ratio is used to define two boxes per element in the list.
     # if ratio=[2], boxes will be created with ratio 1:2 and 2:1
@@ -51,14 +51,14 @@ model = L(SSD300)(
 )
 
 #TODO: change the optimizer to adam?
-optimizer = L(torch.optim.SGD)(
+""" optimizer = L(torch.optim.SGD)(
     # Tip: Scale the learning rate by batch size! 5e-3 is set for a batch size of 32. use 2*5e-3 if you use 64
     lr=1e-3, momentum=0.9, weight_decay=0.0005
-)
-
-""" optimizer = L(torch.optim.Adam)(
-    lr=1e-3, betas=(0.9, 0.999), weight_decay=0.0005
 ) """
+
+optimizer = L(torch.optim.Adam)(
+    lr=1e-3, betas=(0.9, 0.999), weight_decay=0.0005
+)
 
 schedulers = dict(
     linear=L(LinearLR)(start_factor=0.1, end_factor=1, total_iters=500),
